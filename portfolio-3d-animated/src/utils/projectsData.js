f0909889283f40e4f13f0b8c298bd28da10d1960
Mohd Harish <163094451+Hellkryptonium@users.noreply.github.com>
2025-06-09 23:05:11 +0530

    2nd commit

---
+++ portfolio-3d-animated/src/utils/projectsData.js
@@ -0,0 +1,12 @@
+// src/utils/projectsData.js
+const projectsData = [
+  {
+    title: "Project Alpha",
+    description: "A cool project.",
+    image: "/assets/images/projects/alpha-preview.png",
+    link: "#"
+  },
+  // Add more projects as needed
+];
+
+export { projectsData };
